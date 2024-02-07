a6e5e403082b9fc8ba23a9afc6d7e85ad4da5a43
Marcus <marcus.henriksson@restory.ai>
2024-02-07 08:26:31 +0100

    Initial commit

---
+++ src/main/java/com/marcus/awsWebServiceTest/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.marcus.awsWebServiceTest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+    @GetMapping("/")
+    public String sayHelloWorld() {
+        return "Hello World";
+    }
+}
