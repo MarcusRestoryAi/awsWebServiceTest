9cd2774867a2f5e2f503238a4530bce205ba3be6
Marcus <marcus.henriksson@restory.ai>
2024-02-07 15:30:56 +0100

    Changed Port-nummer till 5000

---
+++ src/main/java/com/marcus/awsWebServiceTest/controller/NewController.java
@@ -0,0 +1,15 @@
+package com.marcus.awsWebServiceTest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class NewController {
+
+    @GetMapping("")
+    public String welcome() {
+        return "Startsida";
+    }
+}
